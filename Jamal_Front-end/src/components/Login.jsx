27c32d14079915aafdaf30e5584d0bc675388a0c
buddhi549 <humbleandwise16@gmail.com>
2023-10-23 11:52:22 +1100

    added some styling

--- Jamal_Front-end/src/components/Login.jsx
+++ Jamal_Front-end/src/components/Login.jsx
@@ -3,32 +3,29 @@
 import React, { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import axios from 'axios';
+import './Login.css';
 
-//new line
 import backgroundImage from '../assets/images/secondphoto.jpg';
+
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [role, setRole] = useState('Student');
-  const [error, setError] = useState('');  // New state to manage error message
+  const [error, setError] = useState('');
 
   const history = useHistory();
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');  // Reset error message on new submit
+    setError('');
 
-    // Send login request to server
     try {
         const config = { headers: { 'Content-Type': 'application/json' }};
         const body = JSON.stringify({ email: username, password, role });
         const response = await axios.post('http://localhost:5000/api/auth/login', body, config);
         
-        // Handle successful login here (e.g., save token, redirect user, etc.)
         console.log('Login successful, received token:', response.data.token);
         
-        // Redirecting based on role
         if(role === 'Tutor') {
             history.push('/tutors/sidenav'); 
         } else if(role === 'Student') {
@@ -37,71 +34,73 @@ function Login() {
             history.push('/AdminDashboard/admin-dashboard');
         }
     } catch (err) {
-        console.log(err.response.data)
         console.error(err.response.data);
         setError('Invalid Credentials');
     }
-};
+  };
 
-//new line
-const containerStyle = {
-  backgroundImage: `url(${backgroundImage})`,
-  backgroundSize: 'cover',  // This will ensure the image covers the entire container
-  backgroundRepeat: 'no-repeat',
-  height: '100vh'  // This will make sure it covers the full viewport height
-};
+  const containerStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
 
   return (
     <div style={containerStyle}>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Role:
-            <select value={role} onChange={(e) => setRole(e.target.value)}>
-              <option value="Student">Student</option>
-              <option value="Tutor">Tutor</option>
-              <option value="Admin">Admin</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            Email:
-            <input 
-              type="text" 
-              value={username} 
-              onChange={(e) => setUsername(e.target.value)} 
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password:
-            <input 
-              type="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-            />
-          </label>
-        </div>
-        
-        {/* Error Message Display */}
-        {error && (
-          <div style={{color: 'red', marginBottom: '10px'}}>
-            {error}
+      <div className="login-container">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Role:
+              <select value={role} onChange={(e) => setRole(e.target.value)}>
+                <option value="Student">Student</option>
+                <option value="Tutor">Tutor</option>
+                <option value="Admin">Admin</option>
+              </select>
+            </label>
           </div>
-        )}
+          <div>
+            <label>
+              Email:
+              <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password:
+              <input 
+                type="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+              />
+            </label>
+          </div>
+          
+          {error && (
+            <div style={{color: 'red', marginBottom: '10px'}}>
+              {error}
+            </div>
+          )}
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
 
         <div>
-          <button type="submit">Login</button>
+          <p>Register for an account</p>
+          <Link to="/student-registration">Student</Link>
+          <span> | </span>
+          <Link to="/tutor-registration">Tutor</Link>
         </div>
-      </form>
-
-      <div>
-        <p>Don't have an account?</p>
-        <Link to="/student-registration">Register as Student</Link>
-        <span> | </span>
-        <Link to="/tutor-registration">Register as Tutor</Link>
       </div>
     </div>
   );
--- Jamal_Front-end/src/components/TutorRegistration.jsx
+++ Jamal_Front-end/src/components/TutorRegistration.jsx
@@ -1,180 +1,12 @@
-// import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
-// import axios from 'axios';
 
-// function TutorRegistration() {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     username: '',
-//     password: '',
-//     email: '',
-//   });
-//   const history = useHistory();
-
-//   const handleChange = (e) => {
-//     setFormData({
-//       ...formData,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-//   const registerTutor = async (tutorData) => {
-//     try {
-//         const config = {
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         };
-//         console.log("Sending Data: ", tutorData); // Log data before sending
-//         const res = await axios.post('http://localhost:5000/api/auth/register/tutor', tutorData, config);
-//         console.log('Registration Response:', res.data);
-//     } catch (err) {
-//         console.error('Registration Error:', err.response.data);
-//     }
-// };
-
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log('Tutor Registered:', formData);
-//     registerTutor(formData);
-//     history.push('/login');
-//   };
-
-//   return (
-//     <div>
-//       <h2>Tutor Registration</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Name:
-//           <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <label>
-//           Username:
-//           <input type="text" name="username" value={formData.username} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <label>
-//           Password:
-//           <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <label>
-//           Email:
-//           <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <button type="submit">Register</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default TutorRegistration;
-
-// import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
-// import axios from 'axios';
-
-// function TutorRegistration() {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     username: '',
-//     password: '',
-//     email: '',
-//     tutorExpertise: [],
-//     tutorAvailability: {
-//       monday: '',
-//       tuesday: '',
-//       wednesday: '',
-//       thursday: '',
-//       friday: ''
-//     }
-//   });
-
-//   const history = useHistory();
-
-//   const handleChange = (e) => {
-//     const { name, value, type, checked } = e.target;
-//     if (type === "checkbox") {
-//       setFormData({
-//         ...formData,
-//         tutorExpertise: checked
-//           ? [...formData.tutorExpertise, value]
-//           : formData.tutorExpertise.filter((subject) => subject !== value)
-//       });
-//     } else if (name.includes("Availability")) {
-//       const day = name.split("-")[0];
-//       setFormData({
-//         ...formData,
-//         tutorAvailability: {
-//           ...formData.tutorAvailability,
-//           [day]: value
-//         }
-//       });
-//     } else {
-//       setFormData({
-//         ...formData,
-//         [name]: value
-//       });
-//     }
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     registerTutor(formData);
-//     history.push('/login');
-//   };
-
-//   const registerTutor = async (tutorData) => {
-//     try {
-//         const config = { headers: { 'Content-Type': 'application/json' }};
-//         await axios.post('http://localhost:5000/api/auth/register/tutor', tutorData, config);
-//     } catch (err) {
-//         console.error(err.response.data);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Tutor Registration</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>Name:<input type="text" name="name" value={formData.name} onChange={handleChange} required /></label>
-//         <br />
-//         <label>Username:<input type="text" name="username" value={formData.username} onChange={handleChange} required /></label>
-//         <br />
-//         <label>Password:<input type="password" name="password" value={formData.password} onChange={handleChange} required /></label>
-//         <br />
-//         <label>Email:<input type="email" name="email" value={formData.email} onChange={handleChange} required /></label>
-//         <br />
-//         <label>Expertise:</label>
-//         <label><input type="checkbox" name="tutorExpertise" value="Maths" onChange={handleChange} /> Maths</label>
-//         <label><input type="checkbox" name="tutorExpertise" value="Science" onChange={handleChange} /> Science</label>
-//         <label><input type="checkbox" name="tutorExpertise" value="IT" onChange={handleChange} /> IT</label>
-//         <label><input type="checkbox" name="tutorExpertise" value="Literature" onChange={handleChange} /> Literature</label>
-//         <label><input type="checkbox" name="tutorExpertise" value="Programming" onChange={handleChange} /> Programming</label>
-//         <br />
-//         <label>Availability:</label>
-//         <label>Monday:<input type="text" name="monday-Availability" value={formData.tutorAvailability.monday} onChange={handleChange} required /></label>
-//         <label>Tuesday:<input type="text" name="tuesday-Availability" value={formData.tutorAvailability.tuesday} onChange={handleChange} required /></label>
-//         <label>Wednesday:<input type="text" name="wednesday-Availability" value={formData.tutorAvailability.wednesday} onChange={handleChange} required /></label>
-//         <label>Thursday:<input type="text" name="thursday-Availability" value={formData.tutorAvailability.thursday} onChange={handleChange} required /></label>
-//         <label>Friday:<input type="text" name="friday-Availability" value={formData.tutorAvailability.friday} onChange={handleChange} required /></label>
-//         <br />
-//         <button type="submit">Register</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default TutorRegistration;
 
 
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import backgroundImage from '../assets/images/secondphoto.jpg';
+import './styles.css';
+
 
 function TutorRegistration() {
   const [formData, setFormData] = useState({
@@ -242,7 +74,7 @@ function TutorRegistration() {
   };
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className="container">
       <h2>Tutor Registration</h2>
       <form onSubmit={handleSubmit}>
         <label>Name:<input type="text" name="name" value={formData.name} onChange={handleChange} required /></label>
